e8c358c0e78463d3509771544de1e8799cd67de5
Yousef Mahmoud <usef.mahmoud@outlook.com>
2025-12-29 21:56:09 +0200

    refactor: add error handling for missing BOT_TOKEN in config

--- src/config.js
+++ src/config.js
@@ -3,3 +3,7 @@ dotenv.config();
 
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const TARGET_TEXT_CHANNEL_ID = process.env.TARGET_TEXT_CHANNEL_ID;
+
+if (!BOT_TOKEN) {
+  throw new Error("Missing BOT_TOKEN in .env file");
+}
